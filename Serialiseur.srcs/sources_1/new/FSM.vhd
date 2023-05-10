50e1be40021eead22905ee61d309e2a9d55e43cf
mohanex <mohamedpack@gmail.com>
2023-05-10 12:02:52 +0200

    Advance FSM code that handles more states

--- Serialiseur.srcs/sources_1/new/FSM.vhd
+++ Serialiseur.srcs/sources_1/new/FSM.vhd
@@ -17,30 +17,49 @@ entity FSM is
     CS_serial : out STD_LOGIC;
     stop_serial : out std_logic;
 
+
     clk : in std_logic;
-    reset : in STD_LOGIC
+    reset : in STD_LOGIC;
+    start : in std_logic
     
     );
 end FSM;
 
 architecture Behavioral of FSM is
-    type FSM_Status is (idle, Recherche, Reception, Incrementation);
-    signal status : FSM_Status;
+    type FSM_Status is (IDLE, Recherche, Reception, Envoi, Incrementation);
+    signal state,next_state : FSM_Status;
 begin
+    -- Registers for data storage
+    signal fifo : std_logic_vector(7 downto 0) := (others => '0');
+    variable data_counter : integer range 0 to 4096 := 0;
+    data_serial <= fifo(0);
+
     process(clk) is
         begin
             if rising_edge(Clk) then
                 if reset = '1' then
-                    State <= idle;
+                    State <= IDLE;
+                    addr_ram <= (others => '0');
+
                 elsif rising_edge(clk) then
+                    state <= next_state;
 
                     case State is
-                        when <state_name> =>
-                            <set_outputs_for_this_state_here>
-                            if <state_change_condition_is_true> then
-                                State <= <next_state_name>;
-                            end if;
-                        ...
+                        when IDLE =>
+                        if reset = '0' then
+                            next_state <= Recherche;
+                        else
+                            next_state <= IDLE;
+                        end if;
+
+
+                        when IDLE => Recherche
+                            R_W_ram <= '1';
+                            CS_ram <= '0';
+                            addr_ram <= std_logic_vector( to_unsigned( data_counter, addr_ram'length)); -- convert my data counter to std_logic_vector (4096)10=(1000000000000)2 par exemple
+                            next_state <= ;
+
+
                     end case;
                 end if;
             end if;
