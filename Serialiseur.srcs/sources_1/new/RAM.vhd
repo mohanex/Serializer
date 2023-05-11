fdeb39caa38c5956fa09bcf8ad727443c0949c53
mohanex <mohamedpack@gmail.com>
2023-05-11 11:20:42 +0200

    modified:   Serialiseur.sim/sim_1/behav/xsim/compile.bat 	modified:   Serialiseur.sim/sim_1/behav/xsim/compile.log 	modified:   Serialiseur.sim/sim_1/behav/xsim/elaborate.bat 	modified:   Serialiseur.sim/sim_1/behav/xsim/simulate.bat 	modified:   Serialiseur.sim/sim_1/behav/xsim/tb_RAM_behav.wdb 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/obj/xsim_0.win64.obj 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/obj/xsim_1.c 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/obj/xsim_1.win64.obj 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/webtalk/.xsim_webtallk.info 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/webtalk/xsim_webtalk.tcl 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.dbg 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.mem 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.reloc 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.rlx 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.rtti 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.type 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsim.xdbg 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsimk.exe 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/tb_RAM_behav/xsimkernel.log 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/ram.vdb 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/tb_ram.vdb 	modified:   Serialiseur.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/xil_defaultlib.rlx 	modified:   Serialiseur.sim/sim_1/behav/xsim/xvhdl.log 	modified:   Serialiseur.sim/sim_1/behav/xsim/xvhdl.pb 	modified:   Serialiseur.srcs/sim_1/new/tb_RAM.vhd 	modified:   Serialiseur.xpr updated ram model to be Bidirectional

--- Serialiseur.srcs/sources_1/new/RAM.vhd
+++ Serialiseur.srcs/sources_1/new/RAM.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.05.2023 11:52:02
--- Design Name: 
--- Module Name: Top_FPGA - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -30,15 +9,12 @@ generic (
     constant addr_width : natural := 12
         );
   Port ( 
-  
-  data_out   : out STD_LOGIC_VECTOR (ram_width-1 DOWNTO 0); -- One line of Data in the ram
-  data_in   : in STD_LOGIC_VECTOR (ram_width-1 DOWNTO 0); -- One line of Data in the ram
+  BI_DATA   : inout STD_LOGIC_VECTOR (ram_width-1 DOWNTO 0); -- One line of Data in the ram
   addr : in STD_LOGIC_VECTOR (addr_width-1 DOWNTO 0); -- address on the ram
   R_W : in STD_LOGIC; --Read/Write signal (1: read / 0:write)
   Reset : in STD_LOGIC; --Reset signal
   clk : in STD_LOGIC; --clock
   nCS : in STD_LOGIC -- chip select(active on 0!!)
-
   );
 end RAM;
 
@@ -47,20 +23,33 @@ architecture Behavioral of RAM is
     type RAM is array (0 to ram_depth-1) of std_logic_vector(ram_width-1 downto 0); -- declare a new type named RAM 
     signal ram_data : RAM := (others => "00000000");  --initialize a ram called ram_data of RAM type full of zeros
 
+    signal data_in : STD_LOGIC_VECTOR (ram_width-1 DOWNTO 0); -- One line of Data in the ram
+    signal data_out : STD_LOGIC_VECTOR (ram_width-1 DOWNTO 0); -- One line of Data in the ram
+
 begin
-    process(clk,Reset,nCS)
+    global_proc : process(clk,Reset,nCS)
         begin
         if (Reset = '1') then 
             ram_data <= (others => "00000000"); -- clear the ram
         elsif (rising_edge(clk)) then
             if (nCS = '0') then -- check the chipselect
-                if (R_W = '0') then -- check if its write
-                    ram_data(to_integer(unsigned(addr))) <= data_in; -- put data into memory onto the adrress given
-                end if;
+                ram_data(to_integer(unsigned(addr))) <= data_in; -- put data into memory onto the adrress given
+                data_out <= ram_data(to_integer(unsigned(addr))); -- pull out data from memory adrress given to data out
+            elsif (nCS = '1') then -- if not chipselect 
+                null; -- skip
             end if;
         end if;
-    end process;
-    data_out <= ram_data(to_integer(unsigned(addr))); -- pull out data from memory adrress given to data out
+    end process global_proc;
+
+    Bidirectional_BUS_proc : process(R_W,BI_DATA)
+        begin
+        if (R_W = '0') then -- check if its write
+            data_in <= BI_DATA;
+        elsif (R_W = '1') then -- check if its read
+            BI_DATA <= data_out;
+        end if;
+    end process Bidirectional_BUS_proc;
+
 end Behavioral;
 
 --initlizing from file 
